a16ea6e43ce6e60a19c5b9206d086be8eacd9869
Richard Walker <digitalsadhu@gmail.com>
2014-09-15 01:08:25 +0200

    Fix path, remove use of require mid file

--- run.js
+++ run.js
@@ -9,32 +9,41 @@ module.exports = function (jinn, cb) {
 
   //update package.json script
   function addPackageJsonScript() {
-    var pjson = require(process.cwd() + '/package.json')
-    pjson.scripts = pjson.scripts || {}
-    pjson.scripts.jshint = 'jshint .'
-
-    cb()
+    var packagejson = process.cwd() + '/' + jinn.appname + '/package.json'
+    fs.readFile(packagejson, 'utf8', function (err, content) {
+      if (err) throw err
+      var pjson = JSON.parse(content)
+      pjson.scripts.jshint = 'jshint .'
+      fs.writeFile(packagejson, JSON.stringify(pjson, null, 2), cb)
+    })
   }
 
   //install jshint
   function installJsHint() {
-    var cd = 'cd ' + process.cwd()
+    var cd = 'cd ' + process.cwd() + '/' + jinn.appname
     var install = 'npm install --save-dev jshint'
     exec(cd + ' && ' + install, addPackageJsonScript)
   }
 
   function copyJshintignore() {
-    fs.createReadStream(__dirname + '/templates/.jshintignore')
-      .pipe(fs.createWriteStream(process.cwd() + '/' + jinn.appname + '/.jshintignore'))
-      .on('end', installJsHint)
+    var source = fs.createReadStream(__dirname + '/templates/.jshintignore')
+    var dest   = fs.createWriteStream(process.cwd() + '/' + jinn.appname + '/.jshintignore')
+    source.pipe(dest)
+
+    source.on('end', installJsHint)
   }
 
   //copy jsjintrc
   function copyJshintrc() {
-    var jshintrc = require(__dirname + '/templates/jshintrc.json')
-    if (jinn.options.esnext) jshintrc.esnext = true
-    fs.writeFileSync(process.cwd() + '/' + jinn.appname + '/.jshintrc', JSON.stringify(jshintrc, null, 2))
-    copyJshintignore()
+    var jshintrcSrc = __dirname + '/templates/jshintrc.json'
+    var jshintrcDest = process.cwd() + '/' + jinn.appname + '/.jshintrc'
+
+    fs.readFile(jshintrcSrc, 'utf8', function (err, content) {
+      if (err) throw err
+      var jshint = JSON.parse(content)
+      if (jinn.options.esnext) jshint.esnext = true
+      fs.writeFile(jshintrcDest, JSON.stringify(jshint, null, 2), copyJshintignore)
+    })
   }
 
   copyJshintrc()
